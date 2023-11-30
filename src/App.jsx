6e3025cfe854e1eb68f689a175032e45bf43d7c8
Ana-Maria <anamariaolujic2208@gmail.com>
2023-11-30 18:42:31 +0100

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import List from "./components/List";
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-  ]);
+  const [items, setItems] = useState([]);
 
   const handleChecked = (itemId) => {
     setItems((prevItems) =>
